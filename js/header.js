0f512ccd9c5fc0040a40d14649ac68549b211803
finnmprice <finnprice03@gmail.com>
2025-05-11 20:00:42 -0700

    3d graph fix from desmos update and formatting changes.

--- js/header.js
+++ js/header.js
@@ -1,17 +1,19 @@
 function injectGitHubIcon() {
-    const headerRightContent = document.querySelector('.dcg-header-right-content') || document.querySelector('.align-right-container');
-    
-    if (headerRightContent) {
-      const githubIconUrl = chrome.runtime.getURL('css/github.svg');
-      
-      const isMainDomain = !window.location.href.includes('calculator');
-      // console.log(window.location.href)
-      // console.log(isMainDomain)
-      const iconSize = isMainDomain ? '18px' : '19px';
-      const topMargin = isMainDomain ? '-2px' : '-3px';
-  
-      const style = document.createElement('style');
-      style.textContent = `
+	const headerRightContent =
+		document.querySelector(".dcg-header-right-content") ||
+		document.querySelector(".align-right-container");
+
+	if (headerRightContent) {
+		const githubIconUrl = chrome.runtime.getURL("css/github.svg");
+
+		const isMainDomain = !window.location.href.includes("calculator");
+		// console.log(window.location.href)
+		// console.log(isMainDomain)
+		const iconSize = isMainDomain ? "18px" : "19px";
+		const topMargin = isMainDomain ? "-2px" : "-3px";
+
+		const style = document.createElement("style");
+		style.textContent = `
         .github-icon-link {
           display: inline-flex;
           align-items: center;
@@ -28,24 +30,24 @@ function injectGitHubIcon() {
           margin-top: ${topMargin};
         }
       `;
-      document.head.appendChild(style);
-      
-      const githubLink = document.createElement('a');
-      githubLink.href = 'https://github.com/finnmprice/darkdesmos';
-      githubLink.target = '_blank';
-      githubLink.className = 'github-icon-link';
-      
-      const githubIcon = document.createElement('img');
-      githubIcon.src = githubIconUrl;
-      githubIcon.className = 'github-icon';
-      
-      githubLink.appendChild(githubIcon);
-      headerRightContent.appendChild(githubLink);
-    }
-  }
-  
-  if (document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', injectGitHubIcon);
-  } else {
-    injectGitHubIcon();
-  }
+		document.head.appendChild(style);
+
+		const githubLink = document.createElement("a");
+		githubLink.href = "https://github.com/finnmprice/darkdesmos";
+		githubLink.target = "_blank";
+		githubLink.className = "github-icon-link";
+
+		const githubIcon = document.createElement("img");
+		githubIcon.src = githubIconUrl;
+		githubIcon.className = "github-icon";
+
+		githubLink.appendChild(githubIcon);
+		headerRightContent.appendChild(githubLink);
+	}
+}
+
+if (document.readyState === "loading") {
+	document.addEventListener("DOMContentLoaded", injectGitHubIcon);
+} else {
+	injectGitHubIcon();
+}
